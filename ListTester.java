9c58a8be379cbe7500903c387243547856bcb8c9
watermelon2718 <ellyntran@my.cwi.edu>
2025-04-19 23:14:53 -0600

    edited add methods, scenario 4

--- ListTester.java
+++ ListTester.java
@@ -176,7 +176,7 @@ public class ListTester {
 			// Scenario: 03
 		testSingleElementList(emptyList_addToRearA_A, "emptyList_addToRearA_A", LIST_A, STRING_A);
 			// Scenario: 04
-		testSingleElementList(emptyList_add_A, "emptyList_addA_A", LIST_A, STRING_A)
+		testSingleElementList(emptyList_addA_A, "emptyList_addA_A", LIST_A, STRING_A);
 			// Scenario: 05
 		testSingleElementList(emptyList_addAtIndex0_A, "emptyList_addAtIndex0_A", LIST_A, STRING_A);
 		
@@ -352,6 +352,12 @@ public class ListTester {
 	/** Scenario #04: [] -> add(A) -> [A] 
 	 * @return [A] after add(A)
 	 */
+	private IndexedUnsortedList<Integer> emptyList_addA_A() {
+		IndexedUnsortedList<Integer> list = newList();
+		list.add(0, ELEMENT_A);
+		return list;
+	}
+	private Scenario<Integer> emptyList_addA_A = () -> emptyList_addA_A();
 
 	/** Scenario #05: [] -> add(0, A) -> [A] 
 	 * @return [A] after add(0, A)
